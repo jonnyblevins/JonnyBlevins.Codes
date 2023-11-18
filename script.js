ea11d425f67f2538fac60258fac10106fcf3b275
BlevinsJonny <blevinsjonny@gmail.com>
2023-11-18 15:27:35 -0500

    Overhauled site with new skills for site deployment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const observer = new IntersectionObserver((entries) => {
+/* const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
@@ -11,4 +11,23 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el)); */
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+
+            // Unobserve the element to ensure the animation only occurs once
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+// Observe elements with the class '.hidden'
+const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Observe images with the class '.fade-in'
+const fadeElements = document.querySelectorAll('.fade-in');
+fadeElements.forEach((el) => observer.observe(el));
