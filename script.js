025a78da91eb3fe22b384c79142ea656d26fb036
jonny <blevinsjonny@gmail.com>
2023-11-30 12:53:30 -0500

    "Extra edits just in case."

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-/* const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-        
-    });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el)); */
-
+/*IntersectionObserver is going to watch for the event of scrolling over something*/
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-
-            // Unobserve the element to ensure the animation only occurs once
             observer.unobserve(entry.target);
         }
     });
 });
 
-// Observe elements with the class '.hidden'
+/*When hidden (element) stuff shows up, it adds the show class*/
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-// Observe images with the class '.fade-in'
+/*This makes the fade-in happen seamlessly*/
 const fadeElements = document.querySelectorAll('.fade-in');
 fadeElements.forEach((el) => observer.observe(el));
